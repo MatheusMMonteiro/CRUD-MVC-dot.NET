88ac9392296bed8a19a066f5c3192b79b334186d
Matheus <matheus.malmeida7@gmail.com>
2022-02-01 21:24:40 -0300

    first commit

---
+++ WebMVC/Data/Contexto.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebMVC.Models;
+
+namespace WebMVCMysql.Models.Data
+{
+    public class Contexto: DbContext
+    {
+
+        public Contexto(DbContextOptions<Contexto> options)
+            :base(options)
+        { }
+
+        public DbSet<Usuario> Usuario { get; set; }
+    }
+}
---
+++ WebMVC/Models/Usuario.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebMVC.Models
+{
+    [Table("tb_usuario")]
+    public class Usuario
+    {
+        [Display(Name ="Código")]
+        [Column("Id")]
+        public int Id { get; set; }
+
+        [Display(Name = "Nome")]
+        [Column("Nome")]
+        public string Nome { get; set; }
+    }
+}
